b5f75a59a9702909df0ef0dc86b334f8e07c8764
Douile <25043847+Douile@users.noreply.github.com>
2021-07-23 18:17:43 +0100

    Add support for testing multiple targets

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+TARGETS="$(cat targets)"
+
 REPO="$1"
 if [ -z "$REPO" ]; then
   REPO="rust-build/rust-build.test"
@@ -27,13 +29,16 @@ else
   cd "test"
 fi
 
-sudo docker run \
-  --mount "type=bind,source=$PWD,destination=/github/$REPO" \
-  -e GITHUB_EVENT_DATA="{\"release\":{\"upload_url\":\"\",\"tag_name\":\"test\"}}" \
-  -e GITHUB_REPOSITORY="$REPO" \
-  -e GITHUB_WORKSPACE="/github/$REPO" \
-  -e EXTRA_FILES="$EXTRA_FILES" \
-  -e RUSTTARGET="x86_64-unknown-linux-musl" \
-  -e SRC_DIR="$2" \
-  -e GITHUB_TOKEN="" \
-  --rm -it rust-build
+for target in $TARGETS; do
+  echo "Testing $target"
+  sudo docker run \
+    --mount "type=bind,source=$PWD,destination=/github/$REPO" \
+    -e GITHUB_EVENT_DATA="{\"release\":{\"upload_url\":\"\",\"tag_name\":\"test\"}}" \
+    -e GITHUB_REPOSITORY="$REPO" \
+    -e GITHUB_WORKSPACE="/github/$REPO" \
+    -e EXTRA_FILES="$EXTRA_FILES" \
+    -e RUSTTARGET="$target" \
+    -e SRC_DIR="$2" \
+    -e GITHUB_TOKEN="" \
+    --rm -it rust-build
+done
